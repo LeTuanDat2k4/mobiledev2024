cb274c376f495940f74587f1e397eeca837e4742
Le Tuan Dat <147417920+Vuighe22@users.noreply.github.com>
2024-08-30 13:44:36 +0700

    Update practice 1

--- README.md
+++
@@ -1,9 +0,0 @@
-USTH ICT Android Development 2024
-========================================
-
-* **Your ID**
-22BI13077
-* **Your name**
-Le Tuan Dat
-* **Your class**
-Class 1
---
+++ UsthWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,53 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Weather","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Weathe","onResume");
+    }
+
+    public WeatherActivity() {
+        super();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Weather","OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather","onDestroy");
+    }
+}
